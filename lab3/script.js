58a757ee854b29cafc0037aa10d9559e0c611082
khrviktoriia <viktoriia.khrystan.23@pnu.edu.ua>
2026-03-23 09:41:28 +0200

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,11 +1,11 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
-
-for () {
-
-  
-  if () {
-    
-  } else {
-    
+(function() {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
+})();
