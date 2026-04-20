1195ec7bbf2c0d40162cc3fe33bee394287b2b30
khrviktoriia <viktoriia.khrystan.23@pnu.edu.ua>
2026-04-20 22:27:06 +0300

    Update script.js

--- lab5/script.js
+++ lab5/script.js
@@ -1,1 +1,4 @@
-
+const startBtn = document.getElementById('startBtn');
+const difficultySelect = document.getElementById('difficulty');
+const colorSelect = document.getElementById('colorSelect');
+const gameArea = document.getElementById('gameArea');
